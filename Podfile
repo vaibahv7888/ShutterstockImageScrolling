f0509feb0c08a0cbf7aa63eefa12fb4cac906150
Vaibhav Bangde <Vaibhav_Bangde@Trimble.com>
2019-07-22 16:23:12 +0530

    Committing pods.

---
+++ Podfile
@@ -0,0 +1,23 @@
+platform :ios, '10.0'
+
+target 'UpDay' do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 4.8'
+  pod 'Swinject'
+  pod 'SwinjectAutoregistration', '2.1.1'
+  pod 'AlamofireImage', '~> 3.5'
+  
+  target 'UpDayTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'Swinject'
+    pod 'SwinjectAutoregistration', '2.1.1'
+  end
+
+#  target 'UpDayUITests' do
+#    inherit! :search_paths
+#    # Pods for testing
+#  end
+
+end
---
+++ UpDay/Features/PicturesCollection/Presentation/ViewModel/PicturesViewModelContract.swift
@@ -0,0 +1,19 @@
+//
+//  PicturesViewModelContract.swift
+//  UpDay
+//
+//  Created by Vaibhav Bangde on 7/17/19.
+//  Copyright © 2019 Vaibhav Bangde. All rights reserved.
+//
+
+import UIKit
+
+protocol PicturesViewModelContract {
+    var pageIndex:Int { get set }
+    func getNumberOfItemsInSection() -> Int
+    func getLargeThumnailsPicture(for index:Int) -> Asset?
+    func getPreview1000(for index:Int) -> Asset?
+    func getPictureDescription(for index:Int) -> String?
+    func beginBatchFetchPictures(completion:@escaping (Bool?)->Void)
+    func fetchImage(for url:String, completion:@escaping (UIImage?)->Void) -> ImageRequest?
+}
